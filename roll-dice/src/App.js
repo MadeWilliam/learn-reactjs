c7b68b2ed869582d99882507a86d74118cefe424
pupul.mwm <pupul.mwm@gmail.com>
2020-09-16 16:30:20 +0800

    add roll-dice

---
+++ roll-dice/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import RollDice from './components/roll-dice/RollDice';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <RollDice/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
